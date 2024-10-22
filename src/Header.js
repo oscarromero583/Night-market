423188b79c4f3c97a8c9d1b6445208a10e785302
oscarromero583 <obetaoscar583@gmail.com>
2024-10-22 17:47:05 +0100

    new feature - styled the header section 	modified:   package-lock.json 	modified:   package.json 	modified:   src/Header.css 	modified:   src/Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,39 +1,41 @@
 import React from 'react';
-import './Header.css'
+import "./Header.css";
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import SearchIcon from '@mui/icons-material/Search';
 
-
 function Header() {
     return (
-        <div>
-        
-        <div className="header_logo">
-      <StorefrontIcon fontSize='large' className='header_logoImage'/>
-      <h2 className='header_logoTitle'>Night Market</h2>
-      </div>
+        <div className="header">
 
-      <div className="header_searchbar">
-        <input type="text" className='header_searchInput' />
-        <SearchIcon />
-      </div>
+            {/* Logo Section */}
+            <div className="header__logo">
+                <StorefrontIcon fontSize='large' className='header__logoImage' />
+                <h2 className='header__logoTitle'>Night Market</h2>
+            </div>
 
-      <div className="header_nav">
-        <div className="nav_item">
-            <span className="nav_itemLineOne">Hello My Friend</span>
-            <span className="nav_itemLineTwo">Sign In</span>
-        </div>
-        <div className="nav_item">
-            <span className="nav_itemLineOne">Your</span>
-            <span className="nav_itemLineTwo">Kiosk</span>
-        </div>
-        <div className="nav_item">
-            <ShoppingBasketIcon />
-            <span className="nav_itemLineTwo">0</span>
-        </div>
-      </div>
-      
+            {/* Search Bar */}
+            <div className="header__search">
+                <input type="text" className='header__searchInput' />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+
+            {/* Navigation Section */}
+            <div className="header__nav">
+                <div className="nav__item">
+                    <span className="nav__itemLineOne">Hello My Friend</span>
+                    <span className="nav__itemLineTwo">Sign In</span>
+                </div>
+                <div className="nav__item">
+                    <span className="nav__itemLineOne">Your</span>
+                    <span className="nav__itemLineTwo">Kiosk</span>
+                </div>
+                <div className="nav__itemBasket">
+                    <ShoppingBasketIcon />
+                    <span className="nav__basketCount">0</span>
+                </div>
+            </div>
+
         </div>
     );
 }
