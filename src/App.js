79a0e0c943deed2f19f403777a3aa55eddc62a73
oscarromero583 <obetaoscar583@gmail.com>
2024-10-22 11:18:37 +0100

    Initial commit. Night market 	new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/index.css 	new file:   src/index.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header_logo"></div>
+
+      <div className="header_searchbar"></div>
+      
+    </div>
+
+  );
+}
+
+export default App;
