257ceb0ee463892aeaf095e603ef972eb57e2e74
oscarromero583 <obetaoscar583@gmail.com>
2024-10-22 11:19:29 +0100

    new feature - navigation bar, starting 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ function App() {
       <div className="header_logo"></div>
 
       <div className="header_searchbar"></div>
+
+      <div className="header_nav">
+        <div className="nav_item">
+            <span className="nav_itemLineOne">Hello My Friend</span>
+            <span className="nav_itemLineTwo">Sign In</span>
+        </div>
+        <div className="nav_item">
+            <span className="nav_itemLineOne">Your</span>
+            <span className="nav_itemLineTwo">Kiosk</span>
+        </div>
+        <div className="nav_item">
+            <span className="nav_itemLineOne">Basket</span>
+            <span className="nav_itemLineTwo">0</span>
+        </div>
+      </div>
       
     </div>
 
