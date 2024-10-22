bb15262a4ecdb3fbba398a0caaca55d2c4c93427
oscarromero583 <obetaoscar583@gmail.com>
2024-10-22 12:50:40 +0100

    new feature - Header components 	modified:   src/App.js 	new file:   src/Header.css 	new file:   src/Header.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 
 import './App.css';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import SearchIcon from '@mui/icons-material/Search';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <div className="header_logo">
-      <StorefrontIcon fontSize='large' className='header_logoImage'/>
-      <h2 className='header_logoTitle'>Night Market</h2>
-      </div>
-
-      <div className="header_searchbar">
-        <input type="text" className='header_searchInput' />
-        <SearchIcon />
-      </div>
-
-      <div className="header_nav">
-        <div className="nav_item">
-            <span className="nav_itemLineOne">Hello My Friend</span>
-            <span className="nav_itemLineTwo">Sign In</span>
-        </div>
-        <div className="nav_item">
-            <span className="nav_itemLineOne">Your</span>
-            <span className="nav_itemLineTwo">Kiosk</span>
-        </div>
-        <div className="nav_item">
-            <ShoppingBasketIcon />
-            <span className="nav_itemLineTwo">0</span>
-        </div>
-      </div>
-      
+      <Header />
     </div>
 
   );
