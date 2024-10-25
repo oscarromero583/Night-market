e62668e0b531d0dc2a75562b9adeedd30eb03a0e
oscarromero583 <obetaoscar583@gmail.com>
2024-10-25 09:56:18 +0100

    new feature - modified checkout component and added react-router-dom 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/Header.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-
 import './App.css';
 import Header from './Header';
-import Home from './Home'
+import Home from './Home';
+import Checkout from './Checkout';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
-
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import "./Header.css";
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import SearchIcon from '@mui/icons-material/Search';
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
@@ -30,10 +31,13 @@ function Header() {
                     <span className="nav__itemLineOne">Your</span>
                     <span className="nav__itemLineTwo">Kiosk</span>
                 </div>
+                
+                <Link  to="/checkout" style={{ textDecoration: "none"}}>
                 <div className="nav__itemBasket">
                     <ShoppingBasketIcon />
                     <span className="nav__itemLineTwo nav__basketCount">0</span>
                 </div>
+                </Link>
             </div>
 
         </div>
